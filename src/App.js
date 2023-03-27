5a316b48615913d11f547e0a07209632dcf3ac13
Raquel Castillo <raquel.castillo@simpleviewinc.com>
2023-03-26 19:45:34 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         lets Learn React 
         </a>
       </header>
     </div>
